b09be5d2e1a02afafea0ab5adfad6861986c4a0f
Abdul-Hafeez5 <hafeezrahimoon531@gmail.com>
2023-12-24 02:42:41 +0500

    initial Commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="font-nunito w-full bg-red-400 fixed top-0 ">
+      <div className="container mx-auto">Navbar</div>
+    </nav>
+  );
+};
+
+export default Navbar;
